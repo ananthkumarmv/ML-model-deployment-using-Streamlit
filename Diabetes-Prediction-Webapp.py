f0c9351dec06fadf163a83159acf057051aefcae
Ananth <mvananthkumar@gmail.com>
2022-01-11 00:08:19 +0530

    Update Diabetes-Prediction-Webapp.py

--- Diabetes-Prediction-Webapp.py
+++ Diabetes-Prediction-Webapp.py
@@ -6,12 +6,12 @@ Created on Mon Jan 10 23:07:35 2022
 """
 
 import numpy as np
-import pickle
-import streamlit
+import pickle  # used for loading the saved model
+import streamlit as st # used for creating the webpage
 
 
 # loading the saved model
-loaded_model = pickle.load(open('traied_model.sav', 'rb'))
+loaded_model = pickle.load(open('C:/Users/Unbeknownstguy/Documents/GitHub/Projects/Machine_Learning/ML-model-deployment-using-Streamlit/traied_model.sav', 'rb'))
 
 
 # Creating a function for prediction
@@ -36,4 +36,36 @@ def diabetes_prediction(input_data):
         
         
 
+def main():
+    
+    
+    # giviin a title
+    st.title('Diabetes Preiction Web App')
+    
+    
+    # getting the input data from the user
+    
+    Pregnancies = st.text_input('Number of Pregnancies')
+    Glucose = st.text_input('Glucose Level')
+    BloodPressure = st.text_input('Blood Pressure value')
+    SkinThickness = st.text_input('Skin Thickness value')
+    Insulin = st.text_input('Insulin Level')
+    BMI = st.text_input('Body Mass Index Value') 
+    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    Age = st.text_input('Age of the Person')
+    
+    
+    # code for prediction
+    diagnosis = ''
+    
+    # creating a button for prediction
+    
+    if st.button('Diabetes Test Result'):
+        diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
+    
+    
+    st.success(diagnosis)
+    
 
+if __name__ == '__main__':
+    main()
